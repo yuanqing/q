b4f06125639e8ba2dcdccb4da687bcac60cff4b9
Yuan Qing <desk@yuanqing.sg>
2019-06-27 11:53:24 +0800

    Lint

--- src/watch/html.js
+++ src/watch/html.js
@@ -2,9 +2,7 @@ const build = require('../build/html')
 const constants = require('../constants')
 const watch = require('./watch')
 
-const glob = [
-  constants.html.inputGlob
-]
+const glob = [constants.html.inputGlob]
 
 module.exports = function () {
   return {
